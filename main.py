ad90d62c574c1938feb913beb1053eb456ec7ea6
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-18 00:37:26 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+        print("=" * 40)
+    print("Masala: Ro‘yxatdagi har bir sonni uning 3-darajasi (kub) va 2-darajasi (kvadrat) yig‘indisiga aylantiring.")
+    print("=" * 40)
+    print("\nDastur muvaffaqiyatli ishga tushdi!")
+    print("Masala yechildi.")
+
+
+if __name__ == "__main__":
+    main()
